439750d04395ac4d2dad5ccbd03bc81c6f97ea6f
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2025-06-16 00:59:11 +0530

    Add files via upload

--- app/components/animate/FloatingDock.tsx
+++ app/components/animate/FloatingDock.tsx
@@ -20,8 +20,8 @@ import { useEffect, useRef, useState } from "react";
 export const FloatingDock = ({
   items,
   desktopClassName,
-  mobileClassName,
-}: {
+}: // mobileClassName,
+{
   items: { title: string; icon: React.ReactNode }[];
   desktopClassName?: string;
   mobileClassName?: string;
@@ -34,59 +34,59 @@ export const FloatingDock = ({
   );
 };
 
-const FloatingDockMobile = ({
-  items,
-  className,
-}: {
-  items: { title: string; icon: React.ReactNode }[];
-  className?: string;
-}) => {
-  const [open, setOpen] = useState(false);
-  return (
-    <div className={cn("relative block md:hidden", className)}>
-      <AnimatePresence>
-        {open && (
-          <motion.div
-            layoutId="nav"
-            className="absolute bottom-full mb-2 inset-x-0 flex flex-col gap-2"
-          >
-            {items.map((item, idx) => (
-              <motion.div
-                key={item.title}
-                initial={{ opacity: 0, y: 10 }}
-                animate={{
-                  opacity: 1,
-                  y: 0,
-                }}
-                exit={{
-                  opacity: 0,
-                  y: 10,
-                  transition: {
-                    delay: idx * 0.05,
-                  },
-                }}
-                transition={{ delay: (items.length - 1 - idx) * 0.05 }}
-              >
-                <div
-                  key={item.title}
-                  className="h-10 w-10 rounded-full bg-gray-50 dark:bg-neutral-900 flex items-center justify-center"
-                >
-                  <div className="h-4 w-4">{item.icon}</div>
-                </div>
-              </motion.div>
-            ))}
-          </motion.div>
-        )}
-      </AnimatePresence>
-      {/* <button
-        onClick={() => setOpen(!open)}
-        className="h-10 w-10 rounded-full bg-gray-50 dark:bg-neutral-800 flex items-center justify-center"
-      >
-        <IconLayoutNavbarCollapse className="h-5 w-5 text-neutral-500 dark:text-neutral-400" />
-      </button> */}
-    </div>
-  );
-};
+// const FloatingDockMobile = ({
+//   items,
+//   className,
+// }: {
+//   items: { title: string; icon: React.ReactNode }[];
+//   className?: string;
+// }) => {
+//   const [open, setOpen] = useState(false);
+//   return (
+//     <div className={cn("relative block md:hidden", className)}>
+//       <AnimatePresence>
+//         {open && (
+//           <motion.div
+//             layoutId="nav"
+//             className="absolute bottom-full mb-2 inset-x-0 flex flex-col gap-2"
+//           >
+//             {items.map((item, idx) => (
+//               <motion.div
+//                 key={item.title}
+//                 initial={{ opacity: 0, y: 10 }}
+//                 animate={{
+//                   opacity: 1,
+//                   y: 0,
+//                 }}
+//                 exit={{
+//                   opacity: 0,
+//                   y: 10,
+//                   transition: {
+//                     delay: idx * 0.05,
+//                   },
+//                 }}
+//                 transition={{ delay: (items.length - 1 - idx) * 0.05 }}
+//               >
+//                 <div
+//                   key={item.title}
+//                   className="h-10 w-10 rounded-full bg-gray-50 dark:bg-neutral-900 flex items-center justify-center"
+//                 >
+//                   <div className="h-4 w-4">{item.icon}</div>
+//                 </div>
+//               </motion.div>
+//             ))}
+//           </motion.div>
+//         )}
+//       </AnimatePresence>
+//       {/* <button
+//         onClick={() => setOpen(!open)}
+//         className="h-10 w-10 rounded-full bg-gray-50 dark:bg-neutral-800 flex items-center justify-center"
+//       >
+//         <IconLayoutNavbarCollapse className="h-5 w-5 text-neutral-500 dark:text-neutral-400" />
+//       </button> */}
+//     </div>
+//   );
+// };
 
 const FloatingDockDesktop = ({
   items,
