6c32687b262acd5d4be5b4782bf32c74da89ceb5
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2025-06-16 00:03:37 +0530

    Add files via upload

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import Preloader from "./components/preloader/index";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Abhi Portofolio",
+  description: "portofolio",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Preloader>{children}</Preloader>
+      </body>
+    </html>
+  );
+}
