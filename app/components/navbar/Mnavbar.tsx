668016d657dee5f6993bae68f44089005ea3540c
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2025-06-16 12:02:11 +0530

    Update Mnavbar.tsx

--- app/components/navbar/Mnavbar.tsx
+++ app/components/navbar/Mnavbar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import Link from "next/link";
 
