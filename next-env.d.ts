0c46eb4a181009d1cf09af90b846eb24a2f6d424
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2025-06-16 13:33:09 +0530

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,31 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
-declare module "@splidejs/react-splide" {
-  import { FC, ReactNode } from "react";
-  import {
-    Options as SplideOptions,
-    Splide as SplideInstance,
-  } from "@splidejs/splide";
-
-  export interface SplideProps {
-    options?: SplideOptions;
-    hasTrack?: boolean;
-    children?: ReactNode;
-    // Add other props if needed, like extensions or ref handling
-  }
-
-  export interface SplideSlideProps {
-    children?: ReactNode;
-    className?: string;
-  }
-
-  export interface SplideTrackProps {
-    children?: ReactNode;
-    className?: string;
-  }
-
-  export const Splide: FC<SplideProps>;
-  export const SplideSlide: FC<SplideSlideProps>;
-  export const SplideTrack: FC<SplideTrackProps>;
-}
