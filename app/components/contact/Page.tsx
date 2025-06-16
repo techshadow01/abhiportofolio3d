104e0300ad39fd876996410e1f302a5af3041d73
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2025-06-17 01:10:20 +0530

    Update Page.tsx

--- app/components/contact/Page.tsx
+++ app/components/contact/Page.tsx
@@ -8,11 +8,11 @@ import { projects } from "./contactdata";
 
 const Page = () => {
   return (
-    <div id="contact" className="text-white">
-      <h2 className="bg-clip-text text-6xl font-bold text-center text-transparent md:text-7xl pt-16 bg-gradient-to-b from-black/80 to-black/50 dark:bg-gradient-to-b dark:from-white/80 dark:to-white/20 dark:bg-opacity-50 mb-16 max-sm:mb-8">
+    <div id="contact" className="text-white max-sm:h-screen">
+      <h2 className="bg-clip-text text-6xl font-bold text-center text-transparent md:text-7xl pt-16 bg-gradient-to-b from-black/80 to-black/50 dark:bg-gradient-to-b dark:from-white/80 dark:to-white/20 dark:bg-opacity-50 mb-16 max-sm:mb-0">
         Get in touch
       </h2>
-      <div className="flex max-sm:flex-col items-center justify-around">
+      <div className="flex max-sm:flex-col items-center justify-around max-sm:justify-center max-sm:h-full">
         <div className="relative overflow-hidden w-full max-sm:w-fit max-w-[800px] aspect-square max-sm:aspect-auto flex flex-col items-center justify-center gap-3">
           {/* <Spline
             className="absolute w-full scale-90 max-sm:scale-50 aspect-square top-[6%] max-sm:top-[12%]"
