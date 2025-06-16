6fa548ada4e48bcf9d9b3d649ea3767190a36ce4
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2025-06-16 20:29:35 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import Navbar from "./components/navbar/Page";
+// import Navbar from "./components/navbar/Page";
 import Home from "./components/home/Page";
 import Project from "./components/projects/Page";
 import Skills from "./components/skills/Page";
 import Contact from "./components/contact/Page";
 import Experience from "./components/experience/Page";
-import Mnavbar from "./components/navbar/Mnavbar";
+// import Mnavbar from "./components/navbar/Mnavbar";
 import SmoothScroll from "./components/animate/smooth-scroll";
 
 const page = () => {
   return (
     <div>
       <SmoothScroll>
-        <Mnavbar />
-        <Navbar />
+        {/* <Mnavbar />
+        <Navbar /> */}
         <Home />
         <Experience />
         <Skills />
