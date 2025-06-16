2632aa1a6e27be3fc46cf2d04b67001aa024c7dc
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2025-06-17 00:49:45 +0530

    Update Page.tsx

--- app/components/contact/Page.tsx
+++ app/components/contact/Page.tsx
@@ -2,22 +2,37 @@ import React from "react";
 // import Spline from "@splinetool/react-spline";
 import { HoverEffect } from "../animate/contacttile";
 import { projects } from "./contactdata";
+// import Blob from "../../assets/blob.png";
+// import Image from "next/image";
+// import { SparklesCore } from "../animate/sparkles";
 
 const Page = () => {
   return (
     <div id="contact" className="text-white">
-      <h2 className="bg-clip-text text-6xl font-bold text-center text-transparent md:text-7xl pt-16 bg-gradient-to-b from-black/80 to-black/50 dark:bg-gradient-to-b dark:from-white/80 dark:to-white/20 dark:bg-opacity-50 mb-16 max-sm:mb-0">
+      <h2 className="bg-clip-text text-6xl font-bold text-center text-transparent md:text-7xl pt-16 bg-gradient-to-b from-black/80 to-black/50 dark:bg-gradient-to-b dark:from-white/80 dark:to-white/20 dark:bg-opacity-50 mb-16 max-sm:mb-8">
         Get in touch
       </h2>
       <div className="flex max-sm:flex-col items-center justify-around">
-        <div className="relative overflow-hidden w-full max-w-[800px] aspect-square">
+        <div className="relative overflow-hidden w-full max-sm:w-fit max-w-[800px] aspect-square max-sm:aspect-auto flex flex-col items-center justify-center gap-3">
           {/* <Spline
-            className="absolute w-full aspect-square top-[6%] max-sm:top-[12%]"
-            scene="https://prod.spline.design/dgCbMiYRPgcOqPwm/scene.splinecode"
+            className="absolute w-full scale-90 max-sm:scale-50 aspect-square top-[6%] max-sm:top-[12%]"
+            scene="https://prod.spline.design/gXUWzwMKj4N52IqB/scene.splinecode"
           /> */}
+          {/* <Image
+            className="absolute aspect-auto"
+            src={Blob}
+            alt=""
+            width={800}
+            height={800}
+          /> */}
+          <div className="text-9xl max-sm:text-[10vw]">ABHISHEK</div>
+          <div className="h-[1px] w-64 max-sm:w-24 bg-white"></div>
+          <div className="max-sm:text-[2.5vw]">
+            abhishekjangid.3224@gmail.com
+          </div>
         </div>
         {/* <div className="flex flex-col gap-6"> */}
-        <HoverEffect items={projects} />
+        <HoverEffect items={projects} className="max-sm:w-[300px]" />
         {/* </div> */}
       </div>
     </div>
