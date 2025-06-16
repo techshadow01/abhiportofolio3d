4dc5f93fd165118eebdc19439e422aefb19ced6c
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2025-06-16 12:10:35 +0530

    Update aceternity.tsx

--- app/components/animate/aceternity.tsx
+++ app/components/animate/aceternity.tsx
@@ -1,12 +1,11 @@
 "use client";
 import * as React from "react";
-const AceTernityLogo = (props: any) => (
+const AceTernityLogo = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={85.333}
     height={85.333}
     viewBox="0 0 64 64"
-    {...props}
     fill="#fff"
   >
     <path d="M25.9 14.7C22.5 23.4 13 50.3 13 51.1c0 .5 2 .9 4.3.9h4.4l5-13.8 5-13.7 4.7-.3c5.3-.4 5.4-.7 2.1-8.5C37 12.1 36.8 12 32 12c-4.6 0-5.1.3-6.1 2.7z" />
