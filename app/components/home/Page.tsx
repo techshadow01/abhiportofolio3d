26903d30ae4de9f4592f3ffeb99e02e13bc622be
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2025-06-27 19:01:40 +0530

    Update Page.tsx

--- app/components/home/Page.tsx
+++ app/components/home/Page.tsx
@@ -36,8 +36,9 @@ const Page = () => {
               Get in touch
             </Link>
           </button>
-          <a className="group gap-2 bg-white h-12 w-full rounded-full relative z-40 max-sm:z-0 cursor-pointer flex items-center justify-center px-4">
-              <div className="group-hover:-translate-x-3 duration-500 text-black">
+          <a href="https://drive.google.com/file/d/1M9lN3gk4HJUaru7AsUENXo9D9RNeFUOr/view?usp=sharing"
+            target="_blank" className="group gap-2 bg-white h-12 w-full rounded-full relative z-40 max-sm:z-0 cursor-pointer flex items-center justify-center px-4">
+              <div   className="group-hover:-translate-x-3 duration-500 text-black">
                 Resume
               </div>
               <File className="text-black size-5 right-[65px] top-4 ml-2 absolute opacity-0 scale-50 group-hover:opacity-100 group-hover:scale-100 duration-500 ease-in-out" />
