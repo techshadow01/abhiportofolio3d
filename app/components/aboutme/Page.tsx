39b280d16b19cf11434c3ce856ec9242b9c30bad
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2025-09-08 15:17:28 +0530

    Add files via upload

---
+++ app/components/aboutme/Page.tsx
@@ -0,0 +1,139 @@
+"use client";
+import { useRef } from "react";
+import Card from "../animate/Card";
+import CopyEmailButton from "../animate/CopyEmailButton";
+import { Frameworks } from "../animate/Frameworks";
+import Coding from "../../assets/coding-pov.png";
+import Image from "next/image";
+import { motion, useScroll, useTransform } from "motion/react";
+import GlobeImage from "../../assets/globe.png";
+
+const About = () => {
+  const grid2Container = useRef();
+  const { scrollY } = useScroll();
+
+  const y = useTransform(scrollY, [0, 1000], [0, -300]);
+  return (
+    <div className="relative h-[650px] max-sm:h-[1000px] max-w-7xl mx-auto">
+      <motion.section
+        id="about"
+        style={{ y, position: "absolute", top: 0, left: 0, right: 0 }}
+        className="mx-auto"
+      >
+        {/* <h2 className="bg-clip-text text-6xl font-bold text-center text-transparent md:text-7xl pt-16 bg-gradient-to-b from-black/80 to-black/50 dark:bg-gradient-to-b dark:from-white/80 dark:to-white/20 dark:bg-opacity-50 mb-16">
+          About Me
+        </h2> */}
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-6 md:auto-rows-[18rem]  mt-12">
+          {/* Grid 1 */}
+          <div className="flex items-end grid-default-color grid-1">
+            <Image
+              src={Coding}
+              alt=""
+              className="absolute scale-[1.75] -right-[5rem] -top-[1rem] md:scale-[3] md:left-50 md:inset-y-10 lg:scale-[2.5]"
+            />
+            <div className="z-10">
+              <p className="headtext">Hi, I'm Abhishek Jnagid</p>
+              <p className="subtext">
+                Over the years, I have developed my frontend and backend dev
+                skills to deliver dynamic and software and web applications.
+              </p>
+            </div>
+            <div className="absolute inset-x-0 pointer-evets-none -bottom-4 h-1/2 sm:h-1/3 bg-gradient-to-t from-indigo" />
+          </div>
+          {/* Grid 2 */}
+          <div className="grid-default-color grid-2">
+            <div
+              ref={grid2Container}
+              className="flex items-center justify-center w-full h-full"
+            >
+              <p className="flex items-end text-5xl text-gray-500">
+                CODE IS CRAFT
+              </p>
+              <Card
+                style={{ rotate: "75deg", top: "30%", left: "20%" }}
+                text="GRASP"
+                containerRef={grid2Container}
+              />
+              <Card
+                style={{ rotate: "-30deg", top: "60%", left: "45%" }}
+                text="SOLID"
+                containerRef={grid2Container}
+              />
+              <Card
+                style={{ rotate: "90deg", bottom: "30%", left: "70%" }}
+                text="Design Patterns"
+                containerRef={grid2Container}
+              />
+              <Card
+                style={{ rotate: "-45deg", top: "55%", left: "0%" }}
+                text="Design Principles"
+                containerRef={grid2Container}
+              />
+              <Card
+                style={{ rotate: "20deg", top: "10%", left: "38%" }}
+                text="SRP"
+                containerRef={grid2Container}
+              />
+              <Card
+                style={{ rotate: "30deg", top: "70%", left: "70%" }}
+                image="/logos/csharp-pink.png"
+                containerRef={grid2Container}
+              />
+              <Card
+                style={{ rotate: "-45deg", top: "70%", left: "25%" }}
+                image="/logos/dotnet-pink.png"
+                containerRef={grid2Container}
+              />
+              <Card
+                style={{ rotate: "-45deg", top: "5%", left: "10%" }}
+                image="/logos/blazor-pink.png"
+                containerRef={grid2Container}
+              />
+            </div>
+          </div>
+          {/* Grid 3 */}
+          <div className="grid-black-color grid-3">
+            <div className="z-10 w-[50%]">
+              <p className="headtext">Time Zone</p>
+              <p className="subtext">
+                I'm based in India, and open to remote work worldwide
+              </p>
+            </div>
+            {/* <figure className="absolute left-[30%] top-[10%]">
+              <Globe />
+            </figure> */}
+            <Image
+              className=" absolute right-0 bottom-0"
+              src={GlobeImage}
+              alt=""
+            />
+          </div>
+          {/* Grid 4 */}
+          <div className="grid-special-color grid-4">
+            <div className="flex flex-col items-center justify-center gap-4 size-full">
+              <p className="text-center headtext">
+                Do you want to start a project together?
+              </p>
+              <CopyEmailButton />
+            </div>
+          </div>
+          {/* Grid 5 */}
+          <div className="grid-default-color grid-5">
+            <div className="z-10 w-[50%]">
+              <p className="headText">Teck Stack</p>
+              <p className="subtext">
+                I specialize in a variety of languages, frameworks, and tools
+                taht allow me to build robust and scalable applications
+              </p>
+            </div>
+            <div className="absolute inset-y-0 md:inset-y-9 w-full h-full start-[50%] md:scale-125">
+              <Frameworks />
+            </div>
+          </div>
+        </div>
+      </motion.section>
+    </div>
+  );
+};
+
+export default About;
