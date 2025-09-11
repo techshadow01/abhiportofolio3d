8c54fdf79930c1b085cc51cc063112ac84fa62ee
techshadow01 <156097976+techshadow01@users.noreply.github.com>
2025-09-11 10:57:15 +0530

    Update Page.tsx

--- app/components/navbar/Page.tsx
+++ app/components/navbar/Page.tsx
@@ -1,9 +1,25 @@
+"use client";
+import { useEffect, useState } from "react";
 import { SlideTabs } from "../animate/nav";
 const words = ["home", "experience", "skills", "projects", "contact"];
 
 const Page = () => {
+  //last section
+  const [isLastSection, setIsLastSection] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsLastSection(window.scrollY > 6400);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
-    <div className="max-sm:hidden sticky z-40 top-5 flex items-center justify-center w-full text-white">
+    <div
+      className={`max-sm:hidden sticky z-40 top-5 flex items-center justify-center w-full text-white ${
+        isLastSection && "mix-blend-difference"
+      }`}
+    >
       <SlideTabs className="uppercase " labels={words} />
     </div>
   );
